09e1b92fe163a35db6efbc494265c6000eb4e9c6
Alen <alen.figueroa.m@mail.pucv.cl>
2020-05-05 01:25:24 -0400

    Interfaz gráfica completa

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# IA Para lights up
+# IA Para light up
 
+Light up es un puzzle cuyo objetivo es usar ampolletas para iluminar todo el tablero.
+
+## El puzzle
+
+El puzle tiene 2 elementos: Ampolletas y Paredes.
+Las ampolletas iluminan en dirección horizontal y vertical hasta que llegan a una pared.
+Las paredes pueden contener o no números. En caso de que lo hagan, se debe cumplir la restricción de que debe tener esa cantidad de ampolletas adyacentes a esta (No cuentan diagonales).
+
+## El proyecto
+
+El proyecto consiste en una interfaz gráfica y la IA que resuelve el puzle.
+La GUI está hecha en pygame para la visualización y de los pasos que toma la IA para resolverlo.
+
+### Las reglas
+
+Para el desarrollo de la IA se limitó a sólo el uso de reglas, evitando otras técnicas como el backtracking. 
+
+Las reglas propuestas pueden resolver puzles de dificultad fácil y algunos medios en su totalidad:
+
+- Si el número en un cuadrado negro es igual a la cantidad de espacios blancos disponibles adyacentes a este, entonces se inserta una ampolleta en cada uno de estos.
+- Si la cantidad de ampolletas adyacentes a una pared es igual al número que tiene dentro de esta, entonces el resto de espacios se llenan con una x, bloqueando la posibilidad de colocar una ampolleta dentro de estos.
+- Si una pared contiene un 3, entonces se llenan con una x los 4 espacios diagonales a esta.
+- Si un espacio marcado con una x no está iluminado y sólo tiene disponible un espacio (horizontal o vertical) para colocar una ampolleta, en ese espacio se debe colocar una. Cabe destacar que la posición de este espacio puede ser en cualquier distancia, pero no debe estar bloqueado por una pared.
+- Si un espacio está vacío, no iluminado y todas los demás espacios en su fila y columna están iluminados, son una pared o están bloqueados, entonces se coloca una ampolleta en este espacio.
+
+## TODO
+
+- Implementar métodos dentro del tablero para insertar ampolletas, bloquear espacios y verificar completitud del puzle.
+- Separar lógica de la interfaz en archivo separado.
+- Crear el loop principal con verificación de reglas para luego aplicarlas.
--- main.py
+++ main.py
@@ -1,37 +1,96 @@
 import pygame, sys
+from math import floor
+from time import sleep
+
 
 SCREEN_WIDTH = 700
 SCREEN_HEIGHT = 600
 filename = "input.txt"
+pygame.init()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+class Imagenes:
+    imagenes = {}
+    def __init__(self, ancho, largo):
+        
+        ampolleta = pygame.image.load("./img/bulb.jpg")
+        ampolleta = pygame.transform.scale(ampolleta, (floor(ancho-2), floor(largo-2)))
+        Imagenes.imagenes['s-2'] = ampolleta
+
+        ampolleta = pygame.image.load("./img/x.png")
+        ampolleta = pygame.transform.scale(ampolleta, (floor(ancho-2), floor(largo-2)))
+        Imagenes.imagenes['s-1'] = ampolleta
+
+        
+        for i in range(5):
+            aux = pygame.image.load("./img/pared/" + str(i) + ".jpg")
+            aux = pygame.transform.scale(aux, (floor(ancho-2), floor(largo-2)))
+            Imagenes.imagenes[str(i)] = aux
+
+
 
 class Espacio:
     estado: int
     iluminado: bool
+    texto: pygame.font
 
     def __init__(self, estado=0, iluminado=False):
         self.estado = estado
         self.iluminado = iluminado
+        self.font = pygame.font.SysFont('Arial', 25)
 
     def print(self):
         if self.iluminado:
-            print('\033[1;33;40m', end='')
+            print('_', end='')
         if self.estado == 0:
             print('_', end='')
 
+    def draw(self, pos, ancho, largo):
+        #pygame.draw.rect(screen, (255,255,255), pygame.Rect(pos[0], pos[1],ancho,largo))
+        
+        pos_x = 150 + floor(ancho+2)*pos[0]+2
+        pos_y = 100 + floor(largo+2)*pos[1]+2
+
+        #Primero se ilumina
+        if self.iluminado:
+            color = (255,255,0)
+        else:
+            color=(255,255,255)
+        pygame.draw.rect(screen, color, pygame.Rect(pos_x, pos_y,ancho,largo))
+        #Luego se dibuja la imagen
+        print(self.estado)
+        if self.estado == 0:
+            return
+        
+
+        screen.blit(Imagenes.imagenes['s-' + str(self.estado)], ((pos_x, pos_y)))
+        pygame.display.update()
 
 
 
 class Pared:
     numero: int
+    texto: pygame.font
+
     def __init__(self, numero):
         self.numero = numero
+        self.font = pygame.font.SysFont('Arial', 25)
 
     def print(self):
         print('[', end='')
         if self.numero != -1:
             print(self.numero, end='')
         print(']', end='')
-
+    
+    def draw(self, pos, ancho, largo):
+        print(pos)
+        pos_x = 150 + floor(ancho+2)*pos[0]+2
+        pos_y = 100 + floor(largo+2)*pos[1]+2
+        if self.numero == -1:
+            pygame.draw.rect(screen, (0,0,0), pygame.Rect(pos_x, pos_y,ancho,largo))
+        else:
+            screen.blit(Imagenes.imagenes[str(self.numero)], ((pos_x, pos_y)))
+        pygame.display.update()
 
 
 class Tablero:
@@ -69,13 +128,21 @@ class Tablero:
                 self.tablero[i][j].print()
             print('')
 
+    def update(self):
+        ancho = 400/self.ancho
+        largo = 400/self.largo
+        for i in range(self.largo):
+            for j in range(self.ancho):
+                self.tablero[i][j].draw((i,j), ancho-2, largo-2)
+        pygame.display.update()
 
 
 
 def main():
-    pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    
     pygame.display.set_caption("IA light up")
+    screen.fill([255,255,255])
     # el bucle principal del juego
 
     # Se carga los datos del tablero desde archivo
@@ -87,13 +154,32 @@ def main():
 
 
     # Se crean las líneas del tablero
+    ancho = 400/tablero.ancho
+    largo = 400/tablero.largo
+
+    #Se inicializa la clase que contiene las instancias de las imagenes
+    Imagenes(ancho, largo)
+    # Para líneas horizontales
+    for i in range(1, tablero.largo):
+        pygame.draw.line(screen, (230, 30, 30), (150,100 + i*largo), (400+150,100 +  i*largo), 4)
+
+    # Para líneas verticales
+    for i in range(1, tablero.largo):
+        pygame.draw.line(screen, (230, 30, 30), (150 + i*ancho, 100), (150 +  i*ancho, 100 + 400), 4)
     
+
+
+
+    tablero.update()
+    
+    pygame.display.update()
 
     while True:
         # Posibles entradas del teclado y mouse
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+
 
             
 if __name__ == "__main__":
